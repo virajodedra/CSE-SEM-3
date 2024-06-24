5441f4da9ef6f4b5104344c220fd6ce9cf89dd62
Odedra Viraj <141026170+virajodedra@users.noreply.github.com>
2024-06-24 19:02:56 +0530

    Create LAB_5_30.java

---
+++ LAB 5/LAB_5_30.java
@@ -0,0 +1,49 @@
+import java.util.*;
+public class LAB_5_30 {
+//     0. Read two 2x2 matrices and perform addition of matrices into third matrix and
+// print it
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the row  of the matrix FIRST matrix :");
+        int n = sc.nextInt();
+        System.out.println("Enter the column  of the matrix FIRST matrix :");
+        int m = sc.nextInt();
+        int arr1[][] = new int[n][m];
+
+        System.out.println("Enter the row  of the matrix SECOND matrix :");
+        int x = sc.nextInt();
+        System.out.println("Enter the column  of the matrix SECOND matrix :");
+        int y = sc.nextInt();
+        int arr2[][] = new int[x][y];
+
+        int arr3[][] = new int[n][y];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.println("Enter the ["+i+","+j+"] element of the First array");
+                arr1[i][j] = sc.nextInt();
+            }
+            
+        }
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                System.out.println("Enter the ["+i+","+j+"] element of the Second array");
+                arr2[i][j] = sc.nextInt();
+            }
+            
+        }
+
+        if(m == x){
+            for(int i = 0; i < x; i++) {
+                for(int j = 0 ; j < y; j++) {
+                    arr3[i][j] = arr1[i][j] + arr2[i][j];
+                    System.out.print(arr3[i][j] + " ");
+                }
+                System.out.println();
+            }
+
+        }else{
+            System.out.println("Matrices can't be added");
+        }
+    }
+}
