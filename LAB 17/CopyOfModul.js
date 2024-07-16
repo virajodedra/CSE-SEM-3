3968d59399d5fdfd468933688d24f94ca3b5705f
Pruthviraj Chauhan <chauhanpruthviraj309@gmail.com>
2024-07-16 13:31:32 +0530

    Create CopyOfModul.js

---
+++ LAB 17/CopyOfModul.js
@@ -0,0 +1,49 @@
+const path = require('path');
+
+///////////////////////////path/////////////////
+
+const ans = path.normalize('/abc/../xyz/../uvw/../viraj');
+console.log(ans);
+
+
+filePath = "D:\\viraj\\NODE-js\\Modul.js"
+
+ans1 = path.dirname(filePath);
+console.log('The DIRECTORY NAME is :', ans1);
+
+ans2 = path.basename(filePath);
+console.log('The file name  NAME is :', ans2);
+
+ans3 = path.extname(filePath);
+console.log('The extension NAME is :', ans3);
+
+ans4 = path.posix;
+console.log('The extension NAME is :', ans4);
+
+///////////////////////FILEPATH/////////////////////
+
+const fs = require('fs');
+
+fs.exists("D:\\viraj\\NODE-js\\Modul.js",(data) => {
+    console.log(data);
+});
+console.log("Hal Bhura AAVJE !!!!!!");
+
+fs.stat("D:\\viraj\\NODE-js\\Modul.js",(err,data) => {
+    console.log(data);
+    console.log(err);
+});
+console.log("bhur0 STAT ma ho  baki  kai ghate !!!!!!!!!!!!omphooo");
+ 
+
+fs.readFile("D:\\viraj\\NODE-js\\Modul.js",(err,data) => {
+    console.log(data.toString());
+});
+
+fs.readFile("D:\\viraj\\NODE-js\\Modul.js",(err,data) => {
+    fs.writeFile("copyFileDemo.js",data,(err,data1) => {
+        console.log(err);
+        console.log(data1);
+
+    });
+});
