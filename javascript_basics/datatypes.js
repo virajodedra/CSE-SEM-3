451fec8f54fff0c71fbfff2e4910b1ad124b18a9
Odedra Viraj <virajodedra1808@gmail.com>
2025-05-05 14:49:00 +0000

    datatypes...

---
+++ javascript_basics/datatypes.js
@@ -0,0 +1,24 @@
+"use strict" // treate all js code as newer version.
+
+
+let name = "Volt";
+let age = 21;
+let isLoggedIn = true;
+
+
+//premitive :
+// number  => 2 to power 53
+// string => ""
+// Boolean => true/false;
+// null => standalone value 
+// undefined => absence of the value
+//symbol => used to give uniqueness
+
+// object
+console.log(typeof "Volt");
+
+console.log(typeof null); // object type  (many people also says it is the language error !!)
+
+console.log(typeof undefined);  // undefined
+
+
